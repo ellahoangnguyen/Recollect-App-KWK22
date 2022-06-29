9383dd3dc751c98d4d3d430f93054fdf8dd06a5a
Baovi Nguyen <baovinguyen911@gmail.com>
2022-06-29 16:41:02 -0700

    added question storyboards

---
+++ Recollect App KWK22/QuestionFour.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionFour.swift
+//  Recollect App KWK22
+//
+//  Created by Baovi Nguyen on 6/29/22.
+//
+
+import UIKit
+
+class QuestionFour: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

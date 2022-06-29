bf5f1234dcbdabbb5b3768fda5bda8277fe14bf1
Ella Nguyen <nguyenella05@gmail.com>
2022-06-29 11:13:24 -0700

    first edits

---
+++ Recollect App KWK22/GamePage.swift
@@ -0,0 +1,8 @@
+//
+//  GamePage.swift
+//  Recollect App KWK22
+//
+//  Created by Ella Nguyen on 6/29/22.
+//
+
+import Foundation
---
+++ Recollect App KWK22/StartScreen.swift
@@ -0,0 +1,8 @@
+//
+//  StartScreen.swift
+//  Recollect App KWK22
+//
+//  Created by Ella Nguyen on 6/29/22.
+//
+
+import Foundation
